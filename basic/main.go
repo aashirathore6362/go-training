4ceab759c9f4a1867851368734da3442c4dea3fe
aashirathore6362 <aashi@one2n.in>
2025-03-24 18:05:38 +0530

    Basic go-lang programs

---
+++ basic/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Check even or odd program
+func even(x int) bool {
+	if x%2 == 0 {
+		fmt.Println("Given number is even: ")
+		return true
+	}
+	// no var style
+	return false
+}
+
+// Multiplication Tables
+func tables(n int) int {
+	var i, ans int
+	for i = 1; i <= 10; i++ {
+		ans = n * i
+		fmt.Printf("%v * %v = %v \n", n, i, ans)
+	}
+	return ans
+}
+
+//Sum of naturals number
+func Findsum(n int) {
+	var i, ans int
+	for i = 1; i <= n; i++ {
+		ans = ans + i
+	}
+	fmt.Printf("%v \n", ans)
+}
+
+// swap two number, using 3rd variable
+func swap(a int, b int) {
+	var c int
+	fmt.Printf("Before swap a,b : %v %v \n", a, b)
+	c = a
+	a = b
+	b = c
+	fmt.Printf("After swap a,b : %v %v \n", a, b)
+}
+
+//Find the number closest to n and divisible by m
+func Divisibles(n, m int) {
+	// var a,b int
+	a, b := n, n
+	for a%m != 0 && b%m != 0 {
+		a = a - 1
+		b = b + 1
+		// fmt.Print(a, b)
+	}
+
+	if a%m == 0 && b%m == 0 {
+
+		if math.Abs(float64(a)) < math.Abs(float64(b)) {
+			fmt.Print(b)
+		} else if math.Abs(float64(a)) > math.Abs(float64(b)) {
+			fmt.Print(a)
+		}
+		// fmt.Printf("%v",a)
+	} else if a%m == 0 {
+		fmt.Print(a)
+	} else if b%m == 0 {
+		fmt.Print(b)
+	}else {
+	fmt.Print(n)
+	}
+}
+//You are given a cubic dice with 6 faces. All the individual faces have a number printed on them.
+func Oppositeface(n int) {
+	if (n == 1  || n >= 7) {
+		fmt.Printf("in-vaild face")
+	} else {
+	ans:= 7 - n
+	fmt.Print(ans)
+	}
+}
+
+//Nth term of AP from First Two Terms
+func Nthterms(a1, a2, n int) {
+	 Terms:= a1 + (n - 1) * (a2 - a1)
+	 print(Terms)
+}
+
+func main() {
+	fmt.Println(even(2))
+	tables(4)
+	Findsum(5)
+	swap(2, 4)
+	Divisibles(-15, 6)
+	Oppositeface(2)
+	Nthterms(2,3,4)
+}
