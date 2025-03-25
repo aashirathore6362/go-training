7332b7f3d3af38f6d7f1cef0800e16cc289c261e
aashirathore6362 <aashi@one2n.in>
2025-03-25 17:01:10 +0530

    Basic go-lang programs

--- easy/main.go
+++ easy/main.go
@@ -5,18 +5,46 @@ import (
 	"math"
 )
 
+// sum of numbers
 func SumOfNumber(n int) {
 
 	var sum int
-	for n!=0 {
-		ans:= n % 10
+	for n != 0 {
+		ans := n % 10
 		sum = sum + ans
-		n=n/10
+		n = n / 10
 	}
 	fmt.Print(math.Abs(float64(sum)))
+}
+func ReverseDigits(n int) {
+	var num int = 0
+	for n > 0 {
+		num = num*10 + n%10
+		n = n / 10
+	}
+	fmt.Print(num)
+}
+func PowerOfNumber(x, y int) bool {
+	var temp int
+	temp = 1
+	for temp < y {
+		temp = temp * x
+	}
+	return temp == y
+}
+func RecursionOfPower(x, y float64) bool {
+	if x == y {
+		return true
+	} else if x > y {
+		return false
+	} else {
+		return RecursionOfPower(x, y/x)
+	}
 }
 func main() {
 
 	SumOfNumber(-534)
-
+	ReverseDigits(123)
+	fmt.Print(PowerOfNumber(5, 24))
+	fmt.Print(RecursionOfPower(4, 17))
 }
