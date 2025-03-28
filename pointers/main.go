4148a710c6070ab2ec28191552d0b42848152bde
aashirathore6362 <aashi@o2n.in>
2025-03-28 13:05:23 +0530

    Array basic programs.

--- pointers/main.go
+++ pointers/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+//print 5 numbers in array, 1 to 5
+//Append numbers in array using loops.
+
+func printarray() {
+	var arr1 = [10]int{1,2,3,3,4,5}
+	// fmt.Print(arr1)
+	for i := range arr1 {
+		fmt.Printf("%d\n",i)
+	}
+
+	// arr2.Append
+}
+
 func modString(a string) {
 	a = "modified"
 }
@@ -11,4 +24,6 @@ func main() {
 	fmt.Println("Before:", a)
 	modString(a)
 	fmt.Println("After:", &a)
+
+	printarray()
 }
