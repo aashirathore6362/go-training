c7afc5a8e902afc1a965f64bc89b5654fa1a8c7f
aashirathore6362 <aashi@o2n.in>
2025-04-15 19:13:11 +0530

    completed- Basic number filtering using TDD.

--- ramdom-example/main.go
+++ ramdom-example/main.go
@@ -5,11 +5,11 @@ import (
 )
 
 func main() {
-    f(3)
+	f(3)
 }
 
 func f(x int) {
-    fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
-    defer fmt.Printf("defer %d\n", x)
-    f(x - 1)
+	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
+	defer fmt.Printf("defer %d\n", x)
+	f(x - 1)
 }
