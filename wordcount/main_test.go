81aee471aa19f1eee80183963888b9867f5e3913
Aashi Rathore <support.hul@One2Ns-MacBook-Air.local>
2025-05-10 22:58:01 +0530

    Test cases fix accounding to stdin read.

--- wordcount/main_test.go
+++ wordcount/main_test.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"io"
+	"errors"
+	"os"
 	"strings"
 	"testing"
 )
@@ -11,25 +12,22 @@ func TestWordCount(t *testing.T) {
 		name    string
 		options wordCountOptions
 		path    string
-		reader  io.Reader
 		want    FileStat
 		wantErr error
 	}{
-		// {
-		// 	name:    "Dir instead of file.",
-		// 	path:    "testdata",
-		// 	reader:  nil,
-		// 	wantErr: errors.New("is a directory"),
-		// },
-		// {
-		// 	name:    "File does not exist",
-		// 	path:    "testdata/missing.txt",
-		// 	wantErr: errors.New("no such file"),
-		// },
 		{
-			name:   "Test lines, words, chars count for a single file",
-			path:   "testdata/file1.txt",
-			reader: strings.NewReader("testdata/file1.txt"),
+			name:    "Dir instead of file.",
+			path:    "testdata",
+			wantErr: errors.New("is a directory"),
+		},
+		{
+			name:    "File does not exist",
+			path:    "testdata/missing.txt",
+			wantErr: errors.New("no such file"),
+		},
+		{
+			name: "Test lines, words, chars count for a single file",
+			path: "testdata/file1.txt",
 			options: wordCountOptions{
 				isLineCount: true,
 				isWordCount: true,
@@ -41,127 +39,139 @@ func TestWordCount(t *testing.T) {
 				chars: 21,
 			},
 		},
-		// {
-		// 	name: "Test lines, words, chars count for a single file",
-		// 	path: "testdata/file1.txt",
-		// 	options: wordCountOptions{
-		// 		isLineCount: false,
-		// 		isWordCount: true,
-		// 		isCharCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		words: 5,
-		// 		chars: 21,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -l with single match",
-		// 	path: "testdata/file2.txt",
-		// 	options: wordCountOptions{
-		// 		isLineCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		lines: 1,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -w with no matches",
-		// 	path: "testdata/file2.txt",
-		// 	options: wordCountOptions{
-		// 		isWordCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		words: 0,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -c with matches",
-		// 	path: "testdata/file1.txt",
-		// 	options: wordCountOptions{
-		// 		isCharCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		chars: 21,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -c with multiple matches",
-		// 	path: "testdata/file3.txt",
-		// 	options: wordCountOptions{
-		// 		isCharCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		chars: 68,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -lc with multiple matches",
-		// 	path: "testdata/file3.txt",
-		// 	options: wordCountOptions{
-		// 		isLineCount: true,
-		// 		isCharCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		lines: 8,
-		// 		chars: 68,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -wc with multiple matches",
-		// 	path: "testdata/file3.txt",
-		// 	options: wordCountOptions{
-		// 		isWordCount: true,
-		// 		isCharCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		words: 12,
-		// 		chars: 68,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -lw with multiple matches",
-		// 	path: "testdata/file3.txt",
-		// 	options: wordCountOptions{
-		// 		isLineCount: true,
-		// 		isWordCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		lines: 8,
-		// 		words: 12,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -lwc with multiple matches",
-		// 	path: "testdata/file3.txt",
-		// 	options: wordCountOptions{
-		// 		isLineCount: true,
-		// 		isWordCount: true,
-		// 		isCharCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		lines: 8,
-		// 		words: 12,
-		// 		chars: 68,
-		// 	},
-		// },
-		// {
-		// 	name: "wc -lwc with multiple matches",
-		// 	path: "testdata/file3.txt, testdata/file2.txt",
-		// 	options: wordCountOptions{
-		// 		isLineCount: true,
-		// 		isWordCount: true,
-		// 		isCharCount: true,
-		// 	},
-		// 	want: FileStat{
-		// 		lines: 8,
-		// 		words: 12,
-		// 		chars: 68,
-		// 	},
-		// },
+		{
+			name: "Test lines, words, chars count for a single file",
+			path: "testdata/file1.txt",
+			options: wordCountOptions{
+				isLineCount: false,
+				isWordCount: true,
+				isCharCount: true,
+			},
+			want: FileStat{
+				words: 5,
+				chars: 21,
+			},
+		},
+		{
+			name: "wc -l with single match",
+			path: "testdata/file2.txt",
+			options: wordCountOptions{
+				isLineCount: true,
+			},
+			want: FileStat{
+				lines: 1,
+			},
+		},
+		{
+			name: "wc -w with no matches",
+			path: "testdata/file2.txt",
+			options: wordCountOptions{
+				isWordCount: true,
+			},
+			want: FileStat{
+				words: 0,
+			},
+		},
+		{
+			name: "wc -c with matches",
+			path: "testdata/file1.txt",
+			options: wordCountOptions{
+				isCharCount: true,
+			},
+			want: FileStat{
+				chars: 21,
+			},
+		},
+		{
+			name: "wc -c with multiple matches",
+			path: "testdata/file3.txt",
+			options: wordCountOptions{
+				isCharCount: true,
+			},
+			want: FileStat{
+				chars: 68,
+			},
+		},
+		{
+			name: "wc -lc with multiple matches",
+			path: "testdata/file3.txt",
+			options: wordCountOptions{
+				isLineCount: true,
+				isCharCount: true,
+			},
+			want: FileStat{
+				lines: 8,
+				chars: 68,
+			},
+		},
+		{
+			name: "wc -wc with multiple matches",
+			path: "testdata/file3.txt",
+			options: wordCountOptions{
+				isWordCount: true,
+				isCharCount: true,
+			},
+			want: FileStat{
+				words: 12,
+				chars: 68,
+			},
+		},
+		{
+			name: "wc -lw with multiple matches",
+			path: "testdata/file3.txt",
+			options: wordCountOptions{
+				isLineCount: true,
+				isWordCount: true,
+			},
+			want: FileStat{
+				lines: 8,
+				words: 12,
+			},
+		},
+		{
+			name: "wc -lwc with multiple matches",
+			path: "testdata/file3.txt",
+			options: wordCountOptions{
+				isLineCount: true,
+				isWordCount: true,
+				isCharCount: true,
+			},
+			want: FileStat{
+				lines: 8,
+				words: 12,
+				chars: 68,
+			},
+		},
+		{
+			name: "wc -lwc with multiple matches",
+			path: "testdata/file3.txt",
+			options: wordCountOptions{
+				isLineCount: true,
+				isWordCount: true,
+				isCharCount: true,
+			},
+			want: FileStat{
+				lines: 8,
+				words: 12,
+				chars: 68,
+			},
+		},
 	}
 	for _, tt := range testdatas {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := count(tt.reader, tt.options)
+			f, err := os.Open(tt.path)
+			if err != nil {
+				if tt.wantErr == nil {
+					t.Fatalf("Unexpected error opening file: %v", err)
+				}
+				if !strings.Contains(err.Error(), tt.wantErr.Error()) {
+					t.Fatalf("Expected error to contain %q but got %q", tt.wantErr.Error(), err.Error())
+				}
+				return // Error matched
+			}
+			defer f.Close()
+
+			got, err := countFromReader(f, tt.options)
 
 			if tt.wantErr != nil {
 				if err == nil {
@@ -170,6 +180,7 @@ func TestWordCount(t *testing.T) {
 				if !strings.Contains(err.Error(), tt.wantErr.Error()) {
 					t.Fatalf("Expected error to contain %q but got %q", tt.wantErr.Error(), err.Error())
 				}
+
 				return
 			}
 			if err != nil {
